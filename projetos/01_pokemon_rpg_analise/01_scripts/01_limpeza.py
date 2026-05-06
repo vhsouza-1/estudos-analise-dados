e007e8f6d09740a3176b35cb3a1972407dbd92d7
vhsouza <vinicius.h.zlc@gmail.com>
2026-05-06 17:39:47 -0300

    criado script para adpatar a pokedex tratada para o formato do rpg em questão

--- projetos/01_pokemon_rpg_analise/01_scripts/01_limpeza.py
+++ projetos/01_pokemon_rpg_analise/01_scripts/01_limpeza.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from pathlib import Path
 
+# Subir pokedex original
 entrada1 = Path('../02_raw/pokedex_raw.csv')
 
 df1 = pd.read_csv(entrada1)
 
+# renomear colunas da pokedex
 df1 = df1.rename(columns={
     'ID': 'id',
     'Name': 'name',
@@ -21,20 +23,25 @@ df1 = df1.rename(columns={
     'Generation': 'gen'
 })
 
+# .strip() e .lower() nas colunas 'str'
 for coluna in df1.columns:
     if df1[coluna].dtype == 'str':
         df1[coluna] = df1[coluna].str.lower().str.strip()
 
+# ordenar os valores por id primeiro e depois por gen.
 df1 = df1.sort_values(['id', 'gen']).reset_index(drop=True)
 
+# Alterar a ordem das colunas
 df1 = df1[['id', 'gen', 'name', 'form', 'type_1', 'type_2', 'hp', 'atk', 'def', 'sp_atk', 'sp_def', 'spd', 'bst']]
 
+# Subir pokedex do Bruno (para recuperar coluna pml - pseudos, míticos e lendários)
 entrada2 = Path('../02_raw/pokedex_bruno_raw.csv')
 df2 = pd.read_csv(entrada2)
 
 df1['pml'] = df2['Pseudo/Mítico/ Lendário']
 df1['pml'] = df1['pml'].str.lower().str.strip()
 
+# dicionário para alterar nomes da coluna pml
 dict_pml = {
     'lendário': 'leg',
     'pseudoendário': 'psd_leg',
@@ -44,111 +51,7 @@ dict_pml = {
 df1['pml'] = df1['pml'].replace(dict_pml)
 df1['pml'] = df1['pml'].fillna('')
 
+# Salvar pokedex tratada
 saida = Path('../03_processed/pokedex.csv')
 df1.to_csv(saida, index=False)
-
-input("\nPressione ENTER para sair...")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
---
+++ projetos/01_pokemon_rpg_analise/01_scripts/02_adaptacao_podekedex_rpg.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from pathlib import Path
+import numpy as np
+
+# Ler arquivo pokedex processada
+entrada = Path('../03_processed/pokedex.csv')
+df = pd.read_csv(entrada)
+
+# Converter hp da Pokedex (taxa x0.75)
+df['hp'] = df['hp'] * 0.75
+
+# Converter atk, def, sp_atk, sp_def da Pokedex (taxa * 0.05)
+df['atk'] = df['atk'] * 0.05
+df['sp_atk'] = df['sp_atk'] * 0.05
+df['def'] = df['def'] * 0.05
+df['sp_def'] = df['sp_def'] * 0.05
+
+# Converter spd da Pokedex (taxa * 0.10)
+df['spd'] = df['spd'] * 0.10
+
+# Arredondar para cima e transformar em inteiro
+for stats in df[['hp', 'atk', 'def', 'sp_atk', 'sp_def', 'spd']]:
+    df[stats] = np.floor(df[stats] + 0.5).astype(int)
+
+# dropar BST antigo
+df = df.drop('bst', axis=1)
+
+# Calcular novo BST
+df['bst_rpg'] = df[['hp', 'atk', 'def', 'sp_atk', 'sp_def', 'spd']].sum(axis=1)
+
+
+
+
+
